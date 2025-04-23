e667a6704946b06b13199b2b6c64951c15b3b8b7
preetbhingradiya12 <preetbhingradiya.crawlapps@gmail.com>
2025-04-23 15:27:17 +0530

    Feat: Add organisation CRUD and change role base for manage organisation

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
+import { JwtStrategy } from './lib/role.strategy';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' }, 
+    }),
+    UserModule, 
+  ], 
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService, JwtStrategy]
 })
 export class AuthModule {}
